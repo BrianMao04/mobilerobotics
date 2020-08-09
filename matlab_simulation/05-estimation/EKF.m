5f8e11bda094edd2b38d962fcb0c3243fd1aaaa0
Brian Mao <tdi1986@hotmail.com>
2020-08-09 19:31:44 -0400

    Update EKF.m

--- matlab_simulation/05-estimation/EKF.m
+++ matlab_simulation/05-estimation/EKF.m
@@ -1,19 +1,53 @@
-function [muP,mu,sig,K] = EKF(A,B,C,D,dt,y,u,mu,sig,positionE,measureE,n)
-%This function runs the Extended Kalman Filter with the following inputs:
-%A: state matrix, B: input matrix, C: measurement matrix, D: input
-%measurement matrix, y: measurment model, u: state inputs, n: number of
-%states
+function [mu, S, mup, K] = ekf(mu, S, y, motion_model, measurement_model, linearized_motion_model, linearized_measurement_model, Q, R, u)
+% EFK - Run an iteration of an Extended Kalman Filter
+%
+% Inputs:
+%
+% mu - mean of the state
+% S - covariance matrix
+% y - measurement for the current time-step
+% motion_model - reference to a function for the motion model
+% measurement_model - reference to a function for the measurement model
+% linearized_motion_model - reference to a function for the linearization of the motion model
+% linearized_measurement_model - reference to a function for the linearization of the measurement model
+% Q - measurement disturbance model
+% R - motion disturbance model
+% u - system input at the current timestep
+%
+% Outputs:
+%
+% mu - updated mean
+% S - updated covariance matrix
+% mup - predicted mean
+% K - Kalman gain
 
-%Prediction Update, muP equation based on example
-muP = A*mu + B*u*dt;   %The addition of the input matrix requires the B matrix to be used
-sigP = A*sig*A' + positionE;
+    if ~exist('u', 'var')
+        u = 0;
+    end
 
-%Jacobian matrix to linearize, change this equation for each example
-Ht = [(muP(1))/(sqrt(muP(1)^2 + muP(2)^2)) (muP(2))/(sqrt(muP(1)^2 + muP(2)^2))];
+    %%% Prediction update
+    
+    % Propagate mu through the nonlinear motion model
+    mup = motion_model(mu, u);
+    
+    % Linearize motion model at the predicted mean
+    G = linearized_motion_model(mup, u);
+    
+    % Compute predicted covariance
+    Sp = G*S*G' + R;
 
-% Measurement update
-K = sigP*Ht'*inv(Ht*sigP*Ht'+ measureE);
-mu = muP + K*(y - sqrt(muP(1)^2 + muP(2)^2)); %change h(mu) equations based on example
-sig = (eye(n)-K*Ht)*sigP;
+    
+    %%% Measurement update
+    
+    % Linearize measurement model at the predicted mean
+    H = linearized_measurement_model(mup, u);
+
+    % Compute Kalman gain
+    K = Sp*H'*inv(H*Sp*H'+Q);
+    
+    % Update mean using the nonlinear measurement model
+    mu = mup + K*(y-measurement_model(mup, u));
+    
+    % Update the covariance based on the measurement model
+    S = (eye(length(mu))-K*H)*Sp;
 end
-
